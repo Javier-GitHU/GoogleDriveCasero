52d7f79ddf2d9c6055fa1d30e4f415b942df6fba
Chavier <javiercastrorodriguez2005@gmail.com>
2025-03-07 22:19:10 +0100

    Proyecto actualizado ahora con el cifrado

--- src/main/java/org/example/ManagerFTP.java
+++ src/main/java/org/example/ManagerFTP.java
@@ -2,45 +2,48 @@ package org.example;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.net.ftp.FTPClient;
-
 /**
  *
  * @author javie
  */
 public class ManagerFTP {
-
     private final FTPConection conexionFTP = new FTPConection();
+    private static final String ALGORITMO = "AES";
+    private static final byte[] CLAVE_SECRETA = "1234567890123456".getBytes();
 
     public void subirFichero(String path) {
         new Thread(() -> {
             FTPClient clienteFTP = null;
             try {
                 clienteFTP = conexionFTP.conectar();
-                File archivo = new File(path);
-                try (InputStream is = new FileInputStream(archivo)) {
-                    boolean enviado = clienteFTP.storeFile(archivo.getName(), is);
+                File archivoOriginal = new File(path);
+                File archivoCifrado = new File("encrypted_" + archivoOriginal.getName());
+
+                cifrarArchivo(archivoOriginal, archivoCifrado);
+
+                try (InputStream is = new FileInputStream(archivoCifrado)) {
+                    boolean enviado = clienteFTP.storeFile(archivoCifrado.getName(), is);
                     if (enviado) {
-                        System.out.println("Archivo subido correctamente: " + archivo.getName());
+                        System.out.println("Archivo subido correctamente: " + archivoCifrado.getName());
                     } else {
                         System.err.println("Error al subir el archivo: " + path);
                     }
                 }
+
+                archivoCifrado.delete();
+
             } catch (IOException e) {
                 System.err.println("Error durante la subida del archivo: " + e.getMessage());
                 e.printStackTrace();
             } finally {
-                if (clienteFTP != null && clienteFTP.isConnected()) {
-                    try {
-                        clienteFTP.logout();
-                        clienteFTP.disconnect();
-                    } catch (IOException e) {
-                        System.err.println("Error al cerrar la conexión FTP: " + e.getMessage());
-                        e.printStackTrace();
-                    }
-                }
+                cerrarConexion(clienteFTP);
             }
         }).start();
     }
@@ -50,7 +53,7 @@ public class ManagerFTP {
             FTPClient clienteFTP = null;
             try {
                 clienteFTP = conexionFTP.conectar();
-                boolean eliminado = clienteFTP.deleteFile(nombreFichero);
+                boolean eliminado = clienteFTP.deleteFile("encrypted_" + nombreFichero);
                 if (eliminado) {
                     System.out.println("Archivo eliminado correctamente: " + nombreFichero);
                 } else {
@@ -60,16 +63,42 @@ public class ManagerFTP {
                 System.err.println("Error durante la eliminación del archivo: " + e.getMessage());
                 e.printStackTrace();
             } finally {
-                if (clienteFTP != null && clienteFTP.isConnected()) {
-                    try {
-                        clienteFTP.logout();
-                        clienteFTP.disconnect();
-                    } catch (IOException e) {
-                        System.err.println("Error al cerrar la conexión FTP: " + e.getMessage());
-                        e.printStackTrace();
-                    }
-                }
+                cerrarConexion(clienteFTP);
             }
         }).start();
+    }
+
+    private void cifrarArchivo(File archivoOriginal, File archivoCifrado) throws IOException {
+        try (FileInputStream fis = new FileInputStream(archivoOriginal);
+             FileOutputStream fos = new FileOutputStream(archivoCifrado);
+             CipherOutputStream cos = new CipherOutputStream(fos, obtenerCifrador(Cipher.ENCRYPT_MODE))) {
+            byte[] buffer = new byte[1024];
+            int bytesLeidos;
+            while ((bytesLeidos = fis.read(buffer)) != -1) {
+                cos.write(buffer, 0, bytesLeidos);
+            }
+        }
+    }
+
+    private Cipher obtenerCifrador(int modo) throws IOException {
+        try {
+            Cipher cifrador = Cipher.getInstance(ALGORITMO);
+            cifrador.init(modo, new SecretKeySpec(CLAVE_SECRETA, ALGORITMO));
+            return cifrador;
+        } catch (Exception e) {
+            throw new IOException("Error al inicializar el cifrador: " + e.getMessage(), e);
+        }
+    }
+
+    private void cerrarConexion(FTPClient clienteFTP) {
+        if (clienteFTP != null && clienteFTP.isConnected()) {
+            try {
+                clienteFTP.logout();
+                clienteFTP.disconnect();
+            } catch (IOException e) {
+                System.err.println("Error al cerrar la conexión FTP: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 }
